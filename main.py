5617078c6110823e2215ac3bbd17a98b7ec1c984
24f1000587 <24f1000587@ds.study.iitm.ac.in>
2026-06-21 03:00:20 +0530

    everythimg is d

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ from typing import List
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 class SentimentRequest(BaseModel):
     sentences: List[str]
 
